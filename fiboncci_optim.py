e67fd30d3e925856c9f993b7b06f90fd86114710
Luis Eduardo <luisehica@gmail.com>
2020-07-15 12:02:04 -0500

    Creacion probabilidades.py

--- fiboncci_optim.py
+++ fiboncci_optim.py
@@ -7,28 +7,34 @@ Created on Sat Jul 11 12:04:54 2020
 import time
 import sys 
 
-def fibonacci_recursivo(n,):
+def fibonacci_recursivo(n, count):
+    count += 1
     if n == 0 or n == 1:
-        return 1
-    return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
+        return 1, count
+    return fibonacci_recursivo(n-1, count) + fibonacci_recursivo(n-2, count), count
 
-def fibonacci_dinamico(n, memo = {}):
+def fibonacci_dinamico(n, count, memo = {}):
+    count += 1
     if n == 0 or n == 1:
-        return 1
+        return 1, count
     
     try:
         return memo[n]
     except KeyError:
-        resultado = fibonacci_dinamico(n - 1, memo) + fibonacci_dinamico(n - 2, memo)
+        resultado = fibonacci_dinamico(n - 1, count, memo) + fibonacci_dinamico(n - 2, count, memo)
         memo[n] = resultado
         
-        return resultado
+        return resultado , count
         
 
 if __name__ == '__main__':
     sys.setrecursionlimit(10002)
-    n = 5000
+    n = 6
     comienzo = time.time()
-    fibo = fibonacci_dinamico(n)
+    count = 0
+    count1 = 0
+    fibo, count = fibonacci_recursivo(n,count)
+    fibo1, count1 = fibonacci_dinamico(n, count1)
+    print(f' recursivo {count}, dinamico {count1}')
     final = time.time()
     print(f'El numero de fibonacci de {n} es {fibo}\nEl algoritmo tomo {final-comienzo} segundos')
