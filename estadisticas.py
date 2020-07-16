d9eed1105a25cb81e04f2b21dbe8744fff53f15f
Luis Eduardo <luisehica@gmail.com>
2020-07-16 13:48:55 -0500

    correcion de nombre de desvion_estandar

--- estadisticas.py
+++ estadisticas.py
@@ -13,14 +13,17 @@ def varianza(x):
 
     return acumulador / len(x)
 
-def desv_std(x):
+def desviacion_estandar(x):
     return math.sqrt(varianza(x))
+
+def distribucion_normal(media, sigma, limites=[-inf, inf]):
+    pass
 
 if __name__ == '__main__':
     X = [random.randint(1,21) for i in range(20)]
     mu = media(X)
     sigma_cuadrado = varianza(X)
-    sigma = desv_std(X)
+    sigma = desviacion_estandar(X)
     print(X)
     print(f' media = {mu}')
     print(f' varianza = {sigma_cuadrado}')
