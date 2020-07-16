0f3ab05c7f9a96b53ea4b3ca1478557340566ec7
Luis Eduardo <luisehica@gmail.com>
2020-07-16 13:49:08 -0500

    Finalización simualación

--- barajas.py
+++ barajas.py
@@ -13,13 +13,9 @@ def crear_baraja():
     
     return barajas
 
-
-### Esta linea debe borrarse antes de merge Barajas master
 def obtener_mano(barajas, tamano_mano):
     mano = random.sample(barajas, tamano_mano)
     return mano
-
-    
 
 def main(tamano_mano, intentos):
     barajas = crear_baraja()
@@ -28,26 +24,35 @@ def main(tamano_mano, intentos):
     for _ in range(intentos):
         mano = obtener_mano(barajas, tamano_mano)
         manos.append(mano)
+
     pares = 0
+    corridas = 0
     for mano in manos:
         valores = []
         for carta in mano:
             valores.append(carta[1])
+
         counter = dict(collections.Counter(valores))
+        cartas = sorted(list(counter.keys()))
+        if cartas == list(range(cartas[0],cartas[0]+tamano_mano-1)):
+            corridas +=1
+            break
+
         for var in counter.values():
             if var == 2:
                 pares += 1
                 break
     probabilidad_par = pares / intentos
-    print(f'La probabilidad de obtener un par en una mano de {tamano_mano} barajas es {probabilidad_par}')
-
+    probabilidad_corrida = corridas / intentos
+    print(f'La probabilidad de obtener un par en una mano de {tamano_mano} cartas es {probabilidad_par}')
+    print(f'La probailidad de obtener una carrera en una mano de {tamano_mano} cartas es {probabilidad_corrida}')
 
 if __name__ == '__main__':
     
     barajas = crear_baraja()
     mano = obtener_mano(barajas, 5)
     print(mano)
-    tamano_mano = 5
-    intentos = 10
+    tamano_mano = 6
+    intentos = 1000
 
     main(tamano_mano, intentos)
